b613d6258f9d1d053afdfb16e9636f9f6bb14614
wormam16 <wormam16@b028-24.crio.dauphine.fr>
2019-10-07 14:24:45 +0200

    New Point class

---
+++ shapes/src/main/java/fr/dauphine/ja/wormmsamir/shapes/Point.java
@@ -0,0 +1,17 @@
+package fr.dauphine.ja.wormmsamir.shapes;
+
+/**
+ * Class of a point in 2D
+ */
+public class Point 
+{
+	
+	private int x;
+	private int y;
+	
+    public static void main( String[] args )
+    {
+    	Point p = new Point();
+    	System.out.println(p.x + " " + p.y);
+    }
+}
---
+++ shapes/src/test/java/fr/dauphine/ja/wormmsamir/shapes/TestPoint.java
@@ -0,0 +1,15 @@
+package fr.dauphine.ja.wormmsamir.shapes;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Point.
+ */
+public class TestPoint 
+    extends TestCase
+{
+
+
+}
