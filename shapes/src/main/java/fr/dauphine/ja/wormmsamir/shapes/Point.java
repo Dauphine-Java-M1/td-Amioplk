bc318af0cd317d5820a2988719acb7042336c99a
wormam16 <wormam16@b028-24.crio.dauphine.fr>
2019-10-07 14:46:06 +0200

    Final version of Exercise 1

--- shapes/src/main/java/fr/dauphine/ja/wormmsamir/shapes/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/wormmsamir/shapes/Point.java
@@ -3,15 +3,51 @@ package fr.dauphine.ja.wormmsamir.shapes;
 /**
  * Class of a point in 2D
  */
-public class Point 
-{
-	
+public class Point {
+
 	private int x;
 	private int y;
-	
-    public static void main( String[] args )
-    {
-    	Point p = new Point();
-    	System.out.println(p.x + " " + p.y);
-    }
+	private static int nbPoints = 0;
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Point() {
+		this(0, 0);
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+		setNbPoints(getNbPoints() + 1);
+	}
+
+	public Point(Point p) {
+		this(p.getX(), p.getY());
+	}
+
+	@Override
+	public String toString() {
+		return "(" + Integer.toString(getX()) + "," + Integer.toString(getY()) + ")";
+	}
+
+	public static void main(String[] args) {
+		Point p = new Point();
+		System.out.println(p.x + " " + p.y);
+		System.out.println(p);
+	}
+
+	public static int getNbPoints() {
+		return nbPoints;
+	}
+
+	public static void setNbPoints(int nbPoints) {
+		Point.nbPoints = nbPoints;
+	}
+
 }
---
+++ shapes/src/main/java/fr/dauphine/ja/wormmsamir/shapes/TestPoint.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.wormmsamir.shapes;
+
+/**
+ * Unit test for simple Point.
+ */
+public class TestPoint 
+{
+	public void main() {
+		Point p = new Point();
+		System.out.println(p.getX() + " " + p.getY());
+	}
+
+}
